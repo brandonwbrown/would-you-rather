3701a5e2270646e0c08a58283582e0f352224b14
Brandon Brown <brandonwbrown@gmail.com>
2018-06-25 14:41:54 -0700

    refactor Leaderboard to use const instead of extend

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,50 +1,44 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import User from './User'
 import { Redirect } from 'react-router-dom'
 
 
-class Leaderboard extends Component {
-
-  render() {
-    const { authedUser, users } = this.props
-    return (
-      <div>
-        {authedUser ?
-          <Fragment>
-            <div>
-              <h3 className='center'>Leaders</h3>
-              <ul className='dashboard-list'>
-                {users ? Object.values(users)
-                  .sort((a, b) => {
-                    return(
-                      a.questions.length + Object.keys(a.answers).length >
-                      b.questions.length + Object.keys(b.answers).length ? -1
-                      :
-                      (b.questions.length + Object.keys(b.answers).length >
-                      a.questions.length + Object.keys(a.answers).length ? 1
-                      : 0)
-                    )
-                  })
-                  .map((u, index) => {
-                  var rank = index + 1
-                  return (
-                      <li key={u.id}>
-                        <User id={u.id} user={u} rank={rank}/>
-                      </li>)
-                  })
-                : null
-                }
-              </ul>
-            </div>
-          </Fragment>
-          :
-          <Redirect to='/login' />
-        }
-      </div>
-    )
-  }
-}
+const Leaderboard = ({authedUser, users}) => (
+  <div>
+    {authedUser ?
+      <Fragment>
+        <div>
+          <h3 className='center'>Leaders</h3>
+          <ul className='dashboard-list'>
+            {users ? Object.values(users)
+              .sort((a, b) => {
+                return(
+                  a.questions.length + Object.keys(a.answers).length >
+                  b.questions.length + Object.keys(b.answers).length ? -1
+                  :
+                  (b.questions.length + Object.keys(b.answers).length >
+                  a.questions.length + Object.keys(a.answers).length ? 1
+                  : 0)
+                )
+              })
+              .map((u, index) => {
+              var rank = index + 1
+              return (
+                  <li key={u.id}>
+                    <User id={u.id} user={u} rank={rank}/>
+                  </li>)
+              })
+            : null
+            }
+          </ul>
+        </div>
+      </Fragment>
+      :
+      <Redirect to='/login' />
+    }
+  </div>
+)
 
 function mapStateToProps ({ authedUser, users }) {
   return {
